7a3dc8b3b899f85bd096d5d6249c7aa10aa1cc2d
sthasaroj13 <sarojshrestha651@gmail.com>
2024-04-09 21:39:28 +0545

    responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 function Footer() {
   return (
     <footer className=" bg-black text-gray-400">
-        <div className=" max-w-6xl max-auto grid grid-clos-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+        <div className="  justify-center max-w-6xl max-auto grid grid-clos-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
             <div>
                 <h1 className="text-xl font-bold text-white">About US</h1>
                 <p>Music School is a premier institution dedicated to teaching the art and science of music </p>
