2f79b527ad728967842cfe6db5f9f656d726a9c6
sthasaroj13 <sarojshrestha651@gmail.com>
2024-04-09 20:22:03 +0545

    s

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,27 @@
+'use client'
 import { Meteors } from "@/components/ui/meteors";
 import { Button } from "@/components/ui/moving-border";
 import Link from "next/link";
+import React from "react";
 
-function page() {
+interface ContactForm {
+  email: string;
+  message: string;
+}
+
+function page(): JSX.Element {
+  const [formData, setFormData] = React.useState<ContactForm>({
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <>
       <div className="flex justify-center min-h-screen bg-black py-12 pt-36 bg-gray-900">
@@ -12,12 +31,31 @@ function page() {
             We are here to help with any questions about our courses, programs, or events. Reach out and let us know how we can assist you in your musical journey.
           </p>
 
-          <input type="email" className="w-full bg-gray-800 text-white px-4 py-2 rounded-lg mb-4" placeholder="Your email" /> <br />
-          <textarea placeholder="Your message" cols="30" rows="5" className="w-full bg-gray-800 text-white px-4 py-2 rounded-lg mb-4"></textarea> <br />
+          <input
+            type="email"
+            className="w-full bg-gray-800 text-white px-4 py-2 rounded-lg mb-4"
+            placeholder="Your email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <br />
+
+          <textarea
+            name="message"
+            id=""
+            
+            className="w-full bg-gray-800 text-white px-4 py-2 rounded-lg "
+            placeholder="Write your message here"
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <br />
+
           <Link href={'#'}>
             <Button
               className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-              >
+            >
               Submit
             </Button>
           </Link>
@@ -27,7 +65,7 @@ function page() {
       {/* Meaty part - Meteor effect */}
       <Meteors number={200} />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
